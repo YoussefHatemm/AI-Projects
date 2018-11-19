7ac337d0d549c94c2f5cf1749e6e4d3c64f3561e
Omar Hossam <moar.ahmed@gmail.com>
2018-11-19 18:36:46 +0200

    update

--- logical_agent/axioms.pl
+++ logical_agent/axioms.pl
@@ -25,8 +25,8 @@ jon(X, Y, result(A, S)) :-
     ).
 
 jon(X, Y, result(A, S)) :- 
-    jon(X,Y,S), ( (A = stab, ammo(Z, S), Z > 0) ; (A = refill, dragonStone(X,Y)) ; (A = up, Z is Y + 1, \+approachable(X, Z, S)) ; (A = down, Z is Y-1, \+approachable(X, Z, S)) ; 
-(A = left, Z is X + 1, \+approachable(Z, Y, S)) ; (A = right, Z is X - 1, \+approachable(Z, Y, S))).
+    jon(X,Y,S), ( (A = stab; A = refill) ; (A = up, Z is Y + 1, \+approachable(X, Z, S) , !) ; (A = down, Z is Y-1, \+approachable(X, Z, S), !) ; 
+    (A = left, Z is X + 1, \+approachable(Z, Y, S), !) ; (A = right, Z is X - 1, \+approachable(Z, Y, S), ! ) ).
     
 adjacentToJon(X1,Y1,S) :-
     jon(X,Y,S), ( (X1 = X, Y1 is Y +1) ; (X1 is X + 1, Y1 is Y) ; (X1 is X -1, Y1 is Y); (X1 = X, Y1 is Y -1) ).
@@ -42,14 +42,24 @@ killed(X,Y,result(A,S)) :-
     killed(X,Y,S).
 
 iterative_deepening(Goal, Limit) :-
-    call_with_depth_limit(Goal, Limit,_).
-    
-iterative_deepening(Goal, Limit) :-
-    NewLimit is Limit + 1, 
-    call_with_depth_limit(Goal, NewLimit,_).
+    call_with_depth_limit(Goal, Limit,X),
+    X \= depth_limit_exceeded,
+    !.
 
-ammo(X,result(A,S)) :-
-	(A = refill , maxAmmo(X)) ; (A = stab , ammo(X1, S), X1 is X + 1, X1 > 0).
+iterative_deepening(Goal, Limit) :-
+    NewLimit is Limit + 1,
+    iterative_deepening(Goal, NewLimit).
+
+% ammo(X,result(A,S)) :-
+% 	(A = refill , maxAmmo(X)) ; (A = stab , ammo(X1, S), X1 is X + 1, X1 > 0, jon(X2,Y2,S), adjacentToJon(X2,Y2,S)).
 	
 ammo(X,result(A,S)) :-
-	ammo(X,S), (A \= stab, A \= refill).
+	ammo(X,S), ( (A \= stab, A \= refill) ; (A = stab, ( ( jon(X1,Y1,S), \+adjacentToJon(X1,Y1,S) ) ; X = 0) )).
+
+walkersAlive(N, result(A, S)) :-
+	walkersAlive(N, S), (A = up ; A = down ; A = left; A = right ; A = refill ; (A = stab, jon(X, Y, S), Z1 is X + 1, \+walker(Z1, Y, S), Z2 is X - 1, \+walker(Z2, Y, S), Z3 is Y + 1, 
+		\+walker(X, Z3, S), Z4 is Y - 1, \+walker(X, Z4, S))).
+
+
+walkersAlive(N, result(A, S)) :-
+	M is N + 1, walkersAlive(M, S), A = stab, jon(X, Y, S), (Z1 is X + 1, walker(Z1, Y, S) ; Z2 is X - 1, walker(Z2, Y, S) ; Z3 is Y + 1, walker(X, Z3, S) ; Z4 is Y - 1, walker(X, Z4, S)).
