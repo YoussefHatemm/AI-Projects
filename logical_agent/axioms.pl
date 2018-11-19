ebc98fc86ada542e0742e4c9abd40ddc1fe4cc93
Omar Hossam <moar.ahmed@gmail.com>
2018-11-19 21:14:43 +0200

    hi jojo

--- logical_agent/axioms.pl
+++ logical_agent/axioms.pl
@@ -11,22 +11,26 @@ width(3).
 height(3).
 
 approachable(X, Y, S) :- 
-    \+walker(X,Y,S), \+obstacle(X,Y), height(Z), \+(Y is Z + 1), Y \= 0, width(W), \+(X is W + 1), X \= 0.
+    (\+walker(X,Y,s0) ; killed(X,Y,S)) ,\+obstacle(X,Y), height(Z), \+(Y is Z + 1), Y \= 0, width(W), \+(X is W + 1), X \= 0.
+
+unapproachable(X, Y, S) :-
+    walker(X,Y,s0) ; obstacle(X,Y) ; ( height(Z), Y is Z + 1 ) ; Y = 0 ; (width(W), X is W + 1);  X = 0.
+
 
 % walker(1,2, result(stab, s0)).
 
 jon(X, Y, result(A, S)) :- 
-    approachable(X ,Y, S),
+    jon(X1,Y1,S),
     (
-    (Z is X - 1, jon(Z,Y,S), A = left);
-    (Z is X + 1, jon(Z,Y,S), A = right);
-    (Z is Y + 1, jon(X,Z,S), A = down);
-    (Z is Y - 1, jon(X,Z,S), A = up)
+    (X is X1 + 1, Y = Y1, approachable(X ,Y, S),  A = left);
+    (X is X1+ 1, Y = Y1,  approachable(X ,Y, S), A = right);
+    (X = X1, Y is Y1 - 1, approachable(X ,Y, S), A = down);
+    (X = X1, Y is Y1 + 1, approachable(X ,Y, S), A = up)
     ).
 
 jon(X, Y, result(A, S)) :- 
-    jon(X,Y,S), ( (A = stab; A = refill) ; (A = up, Z is Y + 1, \+approachable(X, Z, S) , !) ; (A = down, Z is Y-1, \+approachable(X, Z, S), !) ; 
-    (A = left, Z is X + 1, \+approachable(Z, Y, S), !) ; (A = right, Z is X - 1, \+approachable(Z, Y, S), ! ) ).
+    jon(X,Y,S), ( (A = stab; A = refill) ; (A = up, Z is Y + 1, unapproachable(X, Z, S) , !) ; (A = down, Z is Y-1, unapproachable(X, Z, S), !) ; 
+    (A = left, Z is X + 1, unapproachable(Z, Y, S), !) ; (A = right, Z is X - 1, unapproachable(Z, Y, S), ! ) ).
     
 adjacentToJon(X1,Y1,S) :-
     jon(X,Y,S), ( (X1 = X, Y1 is Y +1) ; (X1 is X + 1, Y1 is Y) ; (X1 is X -1, Y1 is Y); (X1 = X, Y1 is Y -1) ).
@@ -36,7 +40,7 @@ walker(X,Y, result(A,S)) :-
     walker(X,Y,S), A = stab,!, ( \+adjacentToJon(X,Y,S) ; (ammo(X1,S), X1 = 0 ) ).
 
 killed(X,Y,result(A,S)) :-
-    walker(X,Y,S), adjacentToJon(X,Y,S), ammo(X1,S), X1 > 0.
+    walker(X,Y,S), adjacentToJon(X,Y,S), ammo(X1,S), X1 > 0, A = stab.
 
 killed(X,Y,result(A,S)) :-
     killed(X,Y,S).
@@ -57,9 +61,10 @@ ammo(X,result(A,S)) :-
 	ammo(X,S), ( (A \= stab, A \= refill) ; (A = stab, ( ( jon(X1,Y1,S), \+adjacentToJon(X1,Y1,S) ) ; X = 0) )).
 
 walkersAlive(N, result(A, S)) :-
-	walkersAlive(N, S), (A = up ; A = down ; A = left; A = right ; A = refill ; (A = stab, jon(X, Y, S), Z1 is X + 1, \+walker(Z1, Y, S), Z2 is X - 1, \+walker(Z2, Y, S), Z3 is Y + 1, 
-		\+walker(X, Z3, S), Z4 is Y - 1, \+walker(X, Z4, S))).
+	walkersAlive(N, S), (A = up ; A = down ; A = left; A = right ; A = refill ; 
+        (A = stab, jon(X, Y, S), Z1 is X + 1, (\+walker(Z1, Y, s0) ; killed(Z1, Y, S)) , Z2 is X - 1, (\+walker(Z2, Y, s0); killed(Z2, Y, S)), Z3 is Y + 1, 
+		(\+walker(X, Z3, s0); killed(X, Z3, S)) , Z4 is Y - 1, (\+walker(X, Z4, s0); killed(X,Z4,S) ) )).
 
 
 walkersAlive(N, result(A, S)) :-
-	M is N + 1, walkersAlive(M, S), A = stab, jon(X, Y, S), (Z1 is X + 1, walker(Z1, Y, S) ; Z2 is X - 1, walker(Z2, Y, S) ; Z3 is Y + 1, walker(X, Z3, S) ; Z4 is Y - 1, walker(X, Z4, S)).
+	M is N + 1, walkersAlive(M, S), A = stab, jon(X, Y, S), ( (Z1 is X + 1, walker(Z1, Y, S)) ; (Z2 is X - 1, walker(Z2, Y, S)) ; (Z3 is Y + 1, walker(X, Z3, S)) ; (Z4 is Y - 1, walker(X, Z4, S)) ).
